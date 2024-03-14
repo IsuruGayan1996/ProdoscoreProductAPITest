bc44e1652b17b9e068c06c325987132f10de3737
IsuruGayan1996 <is.weerasinghe@prodoscore.com>
2024-03-14 17:16:28 +0530

    feat: create authorization process

---
+++ src/main/java/com/prodoscore/services/google/utils/Authorization.java
@@ -0,0 +1,50 @@
+package com.prodoscore.services.google.utils;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.gmail.GmailScopes;
+import com.prodoscore.services.google.GMailService;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.util.Set;
+
+public class Authorization {
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param httpTransport The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    public static Credential getCredentials(final NetHttpTransport httpTransport, GsonFactory jsonFactory)
+            throws IOException {
+        // Load client secrets.
+        InputStream in = GMailService.class.getResourceAsStream("/oauth_credential.json");
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + "/oauth_credential.json");
+        }
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(jsonFactory, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, jsonFactory, clientSecrets, Set.of(GmailScopes.GMAIL_SEND, CalendarScopes.CALENDAR))
+                .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        //returns an authorized Credential object.
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+}
