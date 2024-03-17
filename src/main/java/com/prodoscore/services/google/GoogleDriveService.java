70104d6a4c036d4dd373b5d7271c0f14d19312db
IsuruGayan1996 <is.weerasinghe@prodoscore.com>
2024-03-17 14:31:51 +0530

    feat: add new service for add test file to google drive

--- src/main/java/com/prodoscore/services/google/GoogleDriveService.java
+++ src/main/java/com/prodoscore/services/google/GoogleDriveService.java
@@ -1,4 +1,46 @@
 package com.prodoscore.services.google;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.prodoscore.services.google.utils.Authorization;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public class GoogleDriveService {
+    private final Drive service;
+
+    public GoogleDriveService() throws GeneralSecurityException, IOException {
+        NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+        service = new Drive.Builder(httpTransport, jsonFactory, Authorization.getCredentials(httpTransport, jsonFactory))
+                .setApplicationName("Test Driver")
+                .build();
+    }
+
+    public void uploadToDrive() throws IOException {
+        // Upload file photo.jpg on drive.
+        File fileMetadata = new File();
+        fileMetadata.setName("photo.jpg");
+        // File's content.
+        java.io.File filePath = new java.io.File("src/main/resources/files/photo.jpg");
+        // Specify media type and file-path for file.
+        FileContent mediaContent = new FileContent("image/jpeg", filePath);
+        try {
+            File file = service.files().create(fileMetadata, mediaContent)
+                    .setFields("id")
+                    .execute();
+            System.out.println("File ID: " + file.getId());
+//            return file.getId();
+        } catch (GoogleJsonResponseException e) {
+            // TODO(developer) - handle error appropriately
+            System.err.println("Unable to upload file: " + e.getDetails());
+            throw e;
+        }
+    }
 }
